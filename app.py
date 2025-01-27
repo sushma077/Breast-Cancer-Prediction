ad58bbfca0be201b301ed179abb2316f5cca51e8
sushma077 <sushma0740@gmail.com>
2025-01-27 19:49:00 +0530

    Added files

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+import pandas as pd
+import numpy as np
+import pickle
+import os
+
+app=Flask(__name__)
+
+classifier=pickle.load(open("Breast-Cancer-Prediction/cancer_model_prediction.pkl","rb"))
+
+
+@app.route('/')
+
+@app.route('/cancer')
+def home():
+    return render_template("cancer.html")
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    if request.method=='POST':
+        ConcavePoints=float(request.form["ConcavePoints"])
+        Area=float(request.form["Area"])
+        Radius=float(request.form["Radius"])
+        Perimeters=float(request.form["Perimeters"])
+        Concavity=float(request.form["Concavity"])
+        
+
+        input_data=(ConcavePoints,Area, Radius, Perimeters,Concavity)
+        input_data_as_numpy_array=np.asarray(input_data)
+        input_data_reshaped= input_data_as_numpy_array.reshape(1,-1)
+        prediction=classifier.predict(input_data_reshaped)
+
+        if(prediction[0]==1):
+            result="Sorry, you have chances of getting the disease. Please consult the doctor immediately."
+        else:
+            result="No need to fear. You have no dangerous symptoms of the disease."
+
+        return render_template("result.html", result=result)
+    
+
+if __name__=='__main__':
+    app.run(debug=True)
